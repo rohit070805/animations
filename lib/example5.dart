ec751412998a48a75ed7d8c311c2fc776b2383ad
Rohit Kumar <rohitdhankhar7347@gmail.com>
2025-06-01 22:43:42 +0530

    Example 5

---
+++ lib/example5.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+class Example5 extends StatefulWidget {
+  const Example5({super.key});
+
+  @override
+  State<Example5> createState() => _Example5State();
+}
+var defaultwidth = 100.0;
+class _Example5State extends State<Example5> {
+  bool _IsZoomedIn = false;
+  var _width = defaultwidth;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Zoom IN/Zoom OUT",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AnimatedContainer(
+              curve: _IsZoomedIn?Curves.bounceInOut:Curves.bounceOut,
+              width: _IsZoomedIn?MediaQuery.of(context).size.width:200,
+        duration: Duration(milliseconds: 370),
+              child: Image.asset("assets/images/Bg.jpg"),
+
+            ),
+            TextButton(
+              style: ButtonStyle(
+                overlayColor: WidgetStateColor.transparent
+              ),
+                onPressed: (){
+                  setState(() {
+                    _IsZoomedIn = !_IsZoomedIn;
+
+                  });
+                },
+                child: Text(_IsZoomedIn?"Zoom OUT":"Zoom IN",style: TextStyle(fontSize: 20,color: Colors.blue),))
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:animations/example1.dart';
 import 'package:animations/example2.dart';
 import 'package:animations/example3.dart';
 import 'package:animations/example4.dart';
+import 'package:animations/example5.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent),
       ),
-      home: const Example4(),
+      home: const Example5(),
     );
   }
 }
